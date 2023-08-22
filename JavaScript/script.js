3e4c07baace53da125e2f606ffec25786cd1760d
German-4k <127669865+German-4k@users.noreply.github.com>
2023-08-22 09:41:27 +0200

    globale array

---
+++ JavaScript/script.js
@@ -0,0 +1,5 @@
+
+let authos = ['tagesschau', 'tagesschau', 'Name'];
+let imgages = ['./../.jpg', './../.jpg', './../.jpg'];
+let headline = ['Was würd für die 100 Mio Sondervermögen der Bundeswehr eingekauft?']
+
