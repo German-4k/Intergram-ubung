8606e2fc536b27369f2bca067f5b9373b43154ec
German-4k <127669865+German-4k@users.noreply.github.com>
2023-08-22 10:06:46 +0200

    js first version

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,5 +1,41 @@
 
-let authos = ['tagesschau', 'tagesschau', 'Name'];
-let imgages = ['./../.jpg', './../.jpg', './../.jpg'];
-let headline = ['Was würd für die 100 Mio Sondervermögen der Bundeswehr eingekauft?']
+// JSON
+let posts = [
+    {
+        'author': 'Tagesschau',
+        'image': 'img/img1.jpg',
+        'description': 'Text 1 2 3',
+        'location': ''
+    },
 
+    {
+        'author': 'Tagesschau',
+        'image': 'img/img1.jpg',
+        'description': 'Text 1 2 3',
+        'location': 'USA'
+    },
+
+    {
+        'author': 'Tagesschau',
+        'image': 'img/img1.jpg',
+        'description': 'Text 1 2 3',
+        'location': 'Germany'
+    },
+];
+
+    function show(params) {
+        document.getElementById('postcontainer').innerHTML += '';
+        for (let i = 0; index < posts.length; i++) {
+            const post = posts[i];
+
+
+            document.getElementById('postcontainer').innerHTML += `
+            <div>
+                <img src="${psot['image']}">
+                <div>${post['author']}</div>
+                <div>${post['description']}</div>
+                <div>${post['location']}</div>
+            </div>
+            `;
+        }
+    }
