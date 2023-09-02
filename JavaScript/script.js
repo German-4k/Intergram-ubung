f06ed030a692aa44d35c892c61f706717cccaeb3
German-4k <127669865+German-4k@users.noreply.github.com>
2023-09-02 20:45:09 +0200

    css problem gelöst

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -6,7 +6,6 @@ let posts = [
         'image': 'Martin',
         'description': 'Text 1',
         'location': 'France',
-        'comments': ['bla1'],
     },
 
     {
@@ -14,7 +13,6 @@ let posts = [
         'image': 'Paul',
         'description': 'Text 2',
         'location': 'USA',
-        'comments': ['bla2'],
     },
 
     {
@@ -22,12 +20,11 @@ let posts = [
         'image': 'Steffen',
         'description': 'Text 3',
         'location': 'Germany',
-        'comments': ['bla3'],
     },
 ];
-
+    // macht nur sin wenn ich mehre funcktionen in einer zusammen fassen möchte
     function inet() {
-       show() 
+        show()
     };
 
     function show() {
@@ -41,7 +38,7 @@ let posts = [
         }      
     };
 
-
+    
     // onload Event aus dem body (HTML-Element)
     function post() {
         document.getElementById('newPost').innerHTML += `
@@ -58,15 +55,37 @@ let posts = [
                 </div>
             </div>
             `;
+    };
+
+    /*  textarea inhalt wird in dem JSON gespeichert
+        sobalt ich auf dem Button klicke 
+    */
+    function abschicken(i) {
+            let post = { 
+                            author: "Steffen", 
+                            image: "Steffen", 
+                            description: "Text 4",
+                            location: "Germany", 
+                        };
+
+        localStorage.setItem("post", JSON.stringify(post));
+
+        };
+
+
+    function abschicken() {
+        
     }
+    
 
 
      // Taplete für Post's
-     function templatePost(post, i) {
+     // werden rechts angezeigt
+    function templatePost(post, i,) {
         return  `
 
             <div class="post-wrapper">
-                 <div class="post-container">
+                <div class="post-container">
                     <div class="post-image">${post['image']}</div>
                     <div class="post-author">${post['author']}</div>
                     <div class="post-description">${post['description']}</div>
@@ -76,27 +95,3 @@ let posts = [
             </div>
         `
     };
-
-
-    // // nach abschicken (button) ins JSON Speichern
-    // function abschicken() {
-    //     // Wert aus dem Textfeld auslesen / abrufen
-    //     let post = document.getElementById('inputPost').value;
-
-    //     // Vorhandene Posts aus localStorage abrufen und in ein Array umwandeln
-    // let existingPosts = JSON.parse(localStorage.getItem('posts') || '[]');
-
-    // // Neuen Post zum Array hinzufügen
-    // existingPosts.push({
-    //     'author': 'Ihr Name oder Username hier',
-    //     'description': postText,
-    //     'location': 'Ort hier, wenn vorhanden'
-    // });
-
-    // // Array wieder in einen String umwandeln und in localStorage speichern
-    // localStorage.setItem('posts', JSON.stringify(existingPosts));
-
-
-    //     // textare-Feld wird nach Speichern im JSON wieder geleert
-    //     document.getElementById('inputPost').value = '';
-    // }
